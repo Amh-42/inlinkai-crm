8bd035a5b693ded387eab6874f546b33bca9a004
Anwar Misbah <96166023+Amh-42@users.noreply.github.com>
2025-04-11 15:49:56 +0300

    req update

--- passenger_wsgi.py
+++ passenger_wsgi.py
@@ -1,14 +1,30 @@
-import sys, os
-import importlib.util
+import sys
+import os
 
-# Add the application directory to the Python path
-INTERP = "/usr/bin/python3"
-if sys.executable != INTERP:
-    os.execl(INTERP, INTERP, *sys.argv)
+# Get the current directory
+CURRENT_DIR = os.path.dirname(os.path.abspath(__file__))
 
-# Path to your app
-app_dir = os.path.dirname(os.path.abspath(__file__))
-sys.path.insert(0, app_dir)
+# Add virtualenv if it exists (cPanel Python App creates this)
+VIRTUALENV = os.path.join(CURRENT_DIR, 'venv')
+if os.path.exists(VIRTUALENV):
+    PYTHON_PATH = os.path.join(VIRTUALENV, 'bin', 'python')
+    if os.path.exists(PYTHON_PATH):
+        # Use virtualenv Python
+        if sys.executable != PYTHON_PATH:
+            os.execl(PYTHON_PATH, PYTHON_PATH, *sys.argv)
+else:
+    # Use system Python if no virtualenv
+    INTERP = "/usr/bin/python3"
+    if sys.executable != INTERP:
+        os.execl(INTERP, INTERP, *sys.argv)
 
-# Import your Flask app from the root directory
+# Add application directory to Python path
+sys.path.insert(0, CURRENT_DIR)
+
+# Create the 'application' object that Passenger looks for
 from app import app as application
+
+# Uncomment this if you need to work around middleware issues
+# def application(environ, start_response):
+#     environ['SCRIPT_NAME'] = ''
+#     return app(environ, start_response)
